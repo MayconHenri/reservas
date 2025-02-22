b52aee90ce1fc93402831f8ecdfb1c66519755b7
MayconHenri <mayconoficial14@hotmail.com>
2025-02-22 17:15:47 -0300

    Ajuste cadastro

--- src/views/Reservation.vue
+++ src/views/Reservation.vue
@@ -11,13 +11,29 @@
 
     <v-sheet class="mx-auto" elevation="0" style="max-width: 100vw">
       <v-slide-group v-model="model" center-active show-arrows>
-        <v-slide-group-item v-for="(day, index) in sevenDays" :key="index" :class="{ 'disabled-day': day.isSunday }"
-          v-slot="{ isSelected, toggle }">
-          <v-card :color="isSelected ? '#343F53' : 'white'" class="ma-2 d-flex flex-column align-center justify-center"
-            height="80" width="50" @click="!day.isSunday && toggle()" elevation="0" style="border: 1px solid gray"
-            :disabled="day.isSunday || day.isDisabled">
-            <div class="d-flex fill-height align-center justify-center flex-column">
-              <p class="text-capitalize text-center font-weight-small" style="margin-bottom: 5px">
+        <v-slide-group-item
+          v-for="(day, index) in sevenDays"
+          :key="index"
+          :class="{ 'disabled-day': day.isSunday }"
+          v-slot="{ isSelected, toggle }"
+        >
+          <v-card
+            :color="isSelected ? '#343F53' : 'white'"
+            class="ma-2 d-flex flex-column align-center justify-center"
+            height="80"
+            width="50"
+            @click="!day.isSunday && toggle()"
+            elevation="0"
+            style="border: 1px solid gray"
+            :disabled="day.isSunday || day.isDisabled"
+          >
+            <div
+              class="d-flex fill-height align-center justify-center flex-column"
+            >
+              <p
+                class="text-capitalize text-center font-weight-small"
+                style="margin-bottom: 5px"
+              >
                 {{ day.dayName.slice(0, 3) }}
               </p>
               <span class="text-center font-weight-small">
@@ -39,10 +55,17 @@
       <v-tabs-window-item :value="1" v-if="!isLoading">
         <v-btn-toggle v-model="morning" class="d-flex" mandatory>
           <div class="scrollable-container">
-            <v-btn v-for="(hourObj, index) in morningHours" :key="index" color="#343F53" class="mr-20 button"
-              rounded="lg" @click="selectHour(hourObj.hour, 'morning')"
-              :class="{ 'selected-btn': isSelected(hourObj.hour, 'morning') }" style="border: 1px solid gray"
-              :disabled="hourObj.isDisabled">
+            <v-btn
+              v-for="(hourObj, index) in morningHours"
+              :key="index"
+              color="#343F53"
+              class="mr-20 button"
+              rounded="lg"
+              @click="selectHour(hourObj.hour, 'morning')"
+              :class="{ 'selected-btn': isSelected(hourObj.hour, 'morning') }"
+              style="border: 1px solid gray"
+              :disabled="hourObj.isDisabled"
+            >
               {{ hourObj.hour }}
             </v-btn>
           </div>
@@ -50,12 +73,24 @@
       </v-tabs-window-item>
 
       <v-tabs-window-item :value="2">
-        <v-btn-toggle v-model="afternoon" class="d-flex" mandatory v-if="!isLoading">
+        <v-btn-toggle
+          v-model="afternoon"
+          class="d-flex"
+          mandatory
+          v-if="!isLoading"
+        >
           <div class="scrollable-container">
-            <v-btn v-for="(hourObj, index) in afternoonHours" :key="index" color="#343F53" class="mr-20 button"
-              rounded="lg" @click="selectHour(hourObj.hour, 'morning')"
-              :class="{ 'selected-btn': isSelected(hourObj.hour, 'afternoon') }" style="border: 1px solid gray"
-              :disabled="hourObj.isDisabled">
+            <v-btn
+              v-for="(hourObj, index) in afternoonHours"
+              :key="index"
+              color="#343F53"
+              class="mr-20 button"
+              rounded="lg"
+              @click="selectHour(hourObj.hour, 'morning')"
+              :class="{ 'selected-btn': isSelected(hourObj.hour, 'afternoon') }"
+              style="border: 1px solid gray"
+              :disabled="hourObj.isDisabled"
+            >
               {{ hourObj.hour }}
             </v-btn>
           </div>
@@ -65,10 +100,17 @@
       <v-tabs-window-item :value="3" v-if="!isLoading">
         <v-btn-toggle v-model="evening" class="d-flex" mandatory>
           <div class="scrollable-container">
-            <v-btn v-for="(hourObj, index) in eveningHours" :key="index" color="#343F53" class="mr-20 button"
-              rounded="lg" @click="selectHour(hourObj.hour, 'morning')"
-              :class="{ 'selected-btn': isSelected(hourObj.hour, 'evening') }" style="border: 1px solid gray"
-              :disabled="hourObj.isDisabled">
+            <v-btn
+              v-for="(hourObj, index) in eveningHours"
+              :key="index"
+              color="#343F53"
+              class="mr-20 button"
+              rounded="lg"
+              @click="selectHour(hourObj.hour, 'morning')"
+              :class="{ 'selected-btn': isSelected(hourObj.hour, 'evening') }"
+              style="border: 1px solid gray"
+              :disabled="hourObj.isDisabled"
+            >
               {{ hourObj.hour }}
             </v-btn>
           </div>
@@ -87,7 +129,6 @@
             <span class="text-body-1 font-weight-light" v-if="item?.name">
               {{ item?.name }}
             </span>
-
           </v-col>
         </v-row>
         <v-row style="padding: 0; margin: 0">
@@ -99,7 +140,8 @@
                   style: "currency",
                   currency: "BRL",
                 })
-              }}</span>
+              }}</span
+            >
           </v-col>
         </v-row>
         <v-divider class="my-4"></v-divider>
@@ -109,15 +151,23 @@
             <span class="text-body-1 font-weight-light">
               {{ selectedFullDate }}
             </span>
-            <span class="text-body-1 font-weight-light" v-if="selectedTime != '00:00'">, <strong> {{ selectedTime
-                }}</strong>
+            <span
+              class="text-body-1 font-weight-light"
+              v-if="selectedTime != '00:00'"
+              >, <strong> {{ selectedTime }}</strong>
             </span>
           </v-col>
         </v-row>
       </v-card-text>
     </v-card>
 
-    <v-btn class="mx-auto my-6" color="#343F53" rounded block @click="this.dialogVisible = true">
+    <v-btn
+      class="mx-auto my-6"
+      color="#343F53"
+      rounded
+      block
+      @click="this.dialogVisible = true"
+    >
       Finalizar
     </v-btn>
     <v-dialog v-model="dialogVisible" max-width="500px" persistent>
@@ -127,8 +177,17 @@
         </v-toolbar>
 
         <v-card-text>
-          <v-text-field v-model="contato.nome" label="Nome Completo" required></v-text-field>
-          <v-text-field v-model="contato.telefone" label="Telefone" required v-mask="maskTelefone"></v-text-field>
+          <v-text-field
+            v-model="contato.nome"
+            label="Nome Completo"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="contato.telefone"
+            label="Telefone"
+            required
+            v-mask="maskTelefone"
+          ></v-text-field>
         </v-card-text>
 
         <v-card-actions>
@@ -141,33 +200,42 @@
     <v-dialog v-model="dialogCadastro" max-width="500px" persistent>
       <v-card>
         <v-toolbar color="#343F53" dark>
-          <v-toolbar-title v-if="status === 'loading'">Cadastrando...</v-toolbar-title>
-          <v-toolbar-title v-if="status === 'success'">Cadastro Concluído</v-toolbar-title>
-          <v-toolbar-title v-if="status === 'error'">Erro no Cadastro</v-toolbar-title>
+          <v-toolbar-title v-if="status === 'loading'"
+            >Cadastrando...</v-toolbar-title
+          >
+          <v-toolbar-title v-if="status === 'success'"
+            >Cadastro Concluído</v-toolbar-title
+          >
+          <v-toolbar-title v-if="status === 'error'"
+            >Erro no Cadastro</v-toolbar-title
+          >
         </v-toolbar>
 
         <v-card-text class="text-center">
           <template v-if="status === 'loading'">
-            <v-progress-circular indeterminate :size="75" :width="6"></v-progress-circular>
-            <br>
+            <v-progress-circular
+              indeterminate
+              :size="75"
+              :width="6"
+            ></v-progress-circular>
+            <br />
             Aguarde um momento!
           </template>
 
           <template v-if="status === 'success'">
             <v-icon color="green" size="75">mdi-check-circle</v-icon>
-            <br>
+            <br />
             Cadastro realizado com sucesso!
           </template>
 
           <template v-if="status === 'error'">
             <v-icon color="red" size="75">mdi-close-circle</v-icon>
-            <br>
+            <br />
             Ocorreu um erro. Tente novamente.
           </template>
         </v-card-text>
       </v-card>
     </v-dialog>
-
   </v-container>
 </template>
 
@@ -175,7 +243,7 @@
 import dayjs from "dayjs"
 import "dayjs/locale/pt-br"
 import timezone from "dayjs/plugin/timezone"
-import { showToastSuccess, showToastError } from "@/utils/toastUtils";
+import { showToastSuccess, showToastError } from "@/utils/toastUtils"
 dayjs.extend(timezone)
 
 import { fetchEvents, createEvent } from "@/services/api.ts"
@@ -232,26 +300,24 @@ export default {
     }
   },
   mounted() {
-
     this.item = JSON.parse(localStorage.getItem("item"))
     if (!this.item) {
-      this.$router.push('/')
+      this.$router.push("/")
     } else {
-
       fetchEvents().then((data) => {
         this.eventos = data
         this.generateTimes()
-        this.isLoading = false;
+        this.isLoading = false
       })
       // Se for domingo, muda para segunda-feira o select da data
-      let todayIndex = new Date().getDay();
-      console.log('today ' + todayIndex);
+      let todayIndex = new Date().getDay()
+      console.log("today " + todayIndex)
       if (todayIndex === 0) {
-        todayIndex = 1;
+        todayIndex = 1
       } else {
-        todayIndex = 0;
+        todayIndex = 0
       }
-      this.model = todayIndex;
+      this.model = todayIndex
     }
   },
   computed: {
@@ -279,7 +345,7 @@ export default {
     },
     selectedFullDate() {
       this.generateTimes()
-      this.selectedTime = '00:00'
+      this.selectedTime = "00:00"
       this.morning = null
       this.afternoon = null
       this.evening = null
@@ -308,84 +374,77 @@ export default {
   methods: {
     async finalizar() {
       if (!this.contato.nome) {
-        showToastError("Preencha o nome!");
-        return;
+        showToastError("Preencha o nome!")
+        return
       }
 
       if (this.contato.telefone.length < 15) {
-        showToastError("Preencha o telefone!");
-        return;
+        showToastError("Preencha o telefone!")
+        return
       }
 
       if (!this.evento.startDateTime) {
-        showToastError("Preencha o horário de início!");
-        return;
+        showToastError("Preencha o horário de início!")
+        return
       }
 
       if (!this.evento.endDateTime) {
-        showToastError("Preencha o horário de término!");
-        return;
+        showToastError("Preencha o horário de término!")
+        return
       }
 
       // Abre o modal e mostra o estado de carregamento
-      this.dialogVisible = false;
+      this.dialogVisible = false
 
-      this.dialogCadastro = true;
-      this.status = "loading";
-
+      this.dialogCadastro = true
+      this.status = "loading"
 
       // Formatação das datas
       this.evento.startDateTime = dayjs(
         this.selectedFullDateForComparison + "T" + this.evento.startDateTime
-      ).format("YYYY-MM-DDTHH:mm:ss");
+      ).format("YYYY-MM-DDTHH:mm:ss")
 
       this.evento.endDateTime = dayjs(
         this.selectedFullDateForComparison + "T" + this.evento.endDateTime
-      ).format("YYYY-MM-DDTHH:mm:ss");
+      ).format("YYYY-MM-DDTHH:mm:ss")
 
       // Definição dos outros campos
-      this.evento.summary = this.item.name;
+      this.evento.summary = this.item.name
       this.evento.description = `Nome: ${this.contato.nome},
     Telefone: ${this.contato.telefone},
     Serviço: ${this.item.name},
-    Valor: R$ ${this.item.price}`;
+    Valor: R$ ${this.item.price}`
 
       try {
-        const response = await createEvent(this.evento);
-        console.log("Evento criado com sucesso:", response);
+        const response = await createEvent(this.evento)
+        console.log("Evento criado com sucesso:", response)
 
         // Atualiza para sucesso no modal
-        this.status = "success";
+        this.status = "success"
 
         // Exibe toast de sucesso
-        toast.success("Evento criado com sucesso!", {
-          autoClose: 3000,
-          position: "top-right",
-          theme: "colored",
-        });
+        showToastSuccess("Evento criado com sucesso!")
 
         // Espera 2 segundos antes de fechar o modal e redirecionar
         setTimeout(() => {
-          this.dialogCadastro = false;
-          this.$router.push("/"); // Navega para a rota desejada
-        }, 2000);
+          this.dialogCadastro = false
+          this.$router.push("/") // Navega para a rota desejada
+        }, 2000)
       } catch (error) {
-        console.error("Erro ao criar evento:", error);
+        console.error("Erro ao criar evento:", error)
 
         // Atualiza para erro no modal
-        this.status = "error";
+        this.status = "error"
+
+        
 
         // Exibe toast de erro
-        toast.error("Erro ao criar o evento. Tente novamente!", {
-          autoClose: 3000,
-          position: "top-right",
-          theme: "colored",
-        });
-
+        showToastError("Erro ao criar o evento. Tente novamente!")
+        
         // Fecha o modal após 2 segundos
         setTimeout(() => {
-          this.dialogCadastro = false;
-        }, 2000);
+          this.dialogCadastro = false
+        }, 2000)
       }
     },
     formatTime(minutes) {
@@ -424,50 +483,61 @@ export default {
           endHour: 19,
           endMinute: 0,
         },
-      ];
+      ]
 
-      const formattedDate = this.selectedFullDateForComparison;
-      const now = dayjs(); // Data e hora atual
-      const isToday = formattedDate === now.format("YYYY-MM-DD"); // Verifica se é o mesmo dia
+      const formattedDate = this.selectedFullDateForComparison
+      const now = dayjs() // Data e hora atual
+      const isToday = formattedDate === now.format("YYYY-MM-DD") // Verifica se é o mesmo dia
 
-      timePeriods.forEach(({ start, end, period, interval, endHour, endMinute }) => {
-        const hoursArray = [];
+      timePeriods.forEach(
+        ({ start, end, period, interval, endHour, endMinute }) => {
+          const hoursArray = []
 
-        for (let h = start; h <= endHour; h++) {
-          for (let m = 0; m < 60; m += interval) {
-            if (h === endHour && m > endMinute) break;
+          for (let h = start; h <= endHour; h++) {
+            for (let m = 0; m < 60; m += interval) {
+              if (h === endHour && m > endMinute) break
 
-            let hour = h < 10 ? `0${h}` : h;
-            let minute = m === 0 ? "00" : m;
-            const hourFormatted = `${hour}:${minute}`;
+              let hour = h < 10 ? `0${h}` : h
+              let minute = m === 0 ? "00" : m
+              const hourFormatted = `${hour}:${minute}`
 
-            const isEventBlocked = this.eventos.some((event) => {
-              const eventDate = dayjs(new Date(event.start.dateTime)).subtract(this.item.time, "minute");
-              const eventEndDate = dayjs(event.end.dateTime);
-              const eventDay = eventDate.format("YYYY-MM-DD");
+              const isEventBlocked = this.eventos.some((event) => {
+                const eventDate = dayjs(
+                  new Date(event.start.dateTime)
+                ).subtract(this.item.time, "minute")
+                const eventEndDate = dayjs(event.end.dateTime)
+                const eventDay = eventDate.format("YYYY-MM-DD")
 
-              const eventStartHour = eventDate.hour();
-              const eventStartMinute = eventDate.minute();
-              const eventEndHour = eventEndDate.hour();
-              const eventEndMinute = eventEndDate.minute();
+                const eventStartHour = eventDate.hour()
+                const eventStartMinute = eventDate.minute()
+                const eventEndHour = eventEndDate.hour()
+                const eventEndMinute = eventEndDate.minute()
 
-              return (
-                eventDay === formattedDate &&
-                (eventStartHour < h || (eventStartHour === h && eventStartMinute <= m)) &&
-                (eventEndHour > h || (eventEndHour === h && eventEndMinute > m))
-              );
-            });
+                return (
+                  eventDay === formattedDate &&
+                  (eventStartHour < h ||
+                    (eventStartHour === h && eventStartMinute <= m)) &&
+                  (eventEndHour > h ||
+                    (eventEndHour === h && eventEndMinute > m))
+                )
+              })
 
-            // Bloqueia horários menores que a hora atual se for o mesmo dia
-            const isPastTime = isToday && (h < now.hour() || (h === now.hour() && m < now.minute()));
+              // Bloqueia horários menores que a hora atual se for o mesmo dia
+              const isPastTime =
+                isToday &&
+                (h < now.hour() || (h === now.hour() && m < now.minute()))
 
-            //teste
+              //teste
 
-            hoursArray.push({ hour: hourFormatted, isDisabled: isEventBlocked || isPastTime });
+              hoursArray.push({
+                hour: hourFormatted,
+                isDisabled: isEventBlocked || isPastTime,
+              })
+            }
           }
+          this[period] = hoursArray
         }
-        this[period] = hoursArray;
-      });
+      )
     },
 
     selectHour(hour, period) {
